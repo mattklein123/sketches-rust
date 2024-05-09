8209df79c3595c9bdc851682b7e49274d379e154
Matt Klein <mklein@bitdrift.io>
2024-05-09 13:01:20 -0600

    make error implement error
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,9 @@
-use std::{fmt, io};
+use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("Invalid argument: {0}")]
     InvalidArgument(&'static str),
+    #[error("IO error: {0}")]
     IoError(io::ErrorKind),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::InvalidArgument(arg) => write!(f, "Invalid argument: {}", arg),
-            Error::IoError(ref cause) => write!(f, "Io Error: {}", cause),
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Error::IoError(error.kind())
-    }
 }
